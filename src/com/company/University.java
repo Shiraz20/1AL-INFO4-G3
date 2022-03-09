fa8dcacb39c12422d026712dee8093685e0c1288
Shiraz Besrour <shiraz.besrour@gmail.com>
2022-03-09 21:17:45 +0100

    "implementation de l'interface University"

---
+++ src/com/company/University.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public interface University {
+
+    public void ajouterEtudiant(Etudiant e);
+
+    public boolean rechercherEtudiant(Etudiant e);
+
+    public boolean rechercherEtudiant(String nom);
+
+    public void supprimerEtudiant(Etudiant e);
+
+    public void displayEtudiants();
+
+    public void trierEtudiantsParId();
+
+    public void trierEtudiantsParNom();
+}

ea3013bced41dbec6859e9aa1ee4eb173fb0c774
Olfa BEN AFIA <olfa.benafia333@gmail.com>
2022-03-10 15:14:59 +0100

    creer un premier etudiant

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Helloooo'");
+        Etudiant e1 = new Etudiant(1, "etud2", "etud2");
+
+
+    }
 
     }
 }

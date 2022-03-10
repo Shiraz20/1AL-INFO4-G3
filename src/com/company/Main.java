2d27273cd39132fe6f7e0aab93b5e5f28772837d
Olfa BEN AFIA <olfa.benafia333@gmail.com>
2022-03-10 15:15:53 +0100

    creer un deuxieme etudiant

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         Etudiant e1 = new Etudiant(1, "etud2", "etud2");
+        Etudiant e2 = new Etudiant(3, "etud1", "etud1");
 
 
     }

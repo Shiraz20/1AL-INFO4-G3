0e74f58e58ff8a9bed64289ab5d3b5041db3ab13
Olfa BEN AFIA <olfa.benafia333@gmail.com>
2022-03-11 13:40:22 +0100

    main version 3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,12 +7,36 @@ public class Main {
 
 
 
+           // Etudiant e1 = new Etudiant(1, "etud2", "etud2");
+           // Etudiant e2 = new Etudiant(3, "etud1", "etud1");
+           // Etudiant e3 = new Etudiant(2, "etud3", "etud3");
+
+            EspritArrayList esprit = new EspritArrayList();
+            esprit.ajouterEtudiant(e1);
+            esprit.ajouterEtudiant(e2);
+            esprit.ajouterEtudiant(e3);
+
+            esprit.trierEtudiantsParId();
+
+            esprit.displayEtudiants();
+
+            EspritVector espVector = new EspritVector();
+            Etudiant e1 = new Etudiant(1,"AABDOU","PRENOM");
+            Etudiant e2 = new Etudiant(2,"KHALIL","BEN JEMAA");
+            Etudiant e3 = new Etudiant(3,"OLFA","BEN AFIA");
+            Etudiant e4 = new Etudiant(4,"SHIRAZ","BESROUR");
+
+
+            espVector.ajouterEtudiant(e1);
+            espVector.ajouterEtudiant(e2);
+            espVector.ajouterEtudiant(e4);
+            espVector.ajouterEtudiant(e3);
+
+            espVector.displayEtudiants();
+
+
+
     }
+
 }
-
-
-
-
-
-
 

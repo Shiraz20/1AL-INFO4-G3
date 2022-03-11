b8dd9c15ac86478cf8dfbf64dbf9ae6f5af72f88
Olfa BEN AFIA <olfa.benafia333@gmail.com>
2022-03-11 13:21:23 +0100

    Modification de la classe main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,10 +4,65 @@ public class Main {
 
     public static void main(String[] args) {
         Etudiant e1 = new Etudiant(1, "etud2", "etud2");
-        Etudiant e2 = new Etudiant(3, "etud1", "etud1");
 
 
-    }
+//        List<String> list = new ArrayList<>();
+//
+//        list.add("test");
+//        list.add("abc");
+//       // list.add(2);
+//        list.add("atest");
+//      //  list.add(3.2f);
+//
+//      //list.sort(c);
+//       Collections.sort(list);
+//
+//       // list.remove(1);
+//
+//      //  System.out.println( list.contains(3.2f));
+//
+////        for (int i = 0; i < list.size(); i++) {
+////            System.out.println(list.get(i));
+////        }
+//        for (Object o : list) {
+//            System.out.println(o);
+//        }
+            //System.out.println(list);
+           // Etudiant e1 = new Etudiant(1, "etud2", "etud2");
+           // Etudiant e2 = new Etudiant(3, "etud1", "etud1");
+           // Etudiant e3 = new Etudiant(2, "etud3", "etud3");
 
+          /*  EspritArrayList esprit = new EspritArrayList();
+            esprit.ajouterEtudiant(e1);
+            esprit.ajouterEtudiant(e2);
+            esprit.ajouterEtudiant(e3);
+
+            esprit.trierEtudiantsParId();
+
+            esprit.displayEtudiants();
+
+            EspritVector espVector = new EspritVector();
+            Etudiant e1 = new Etudiant(1,"AABDOU","PRENOM");
+            Etudiant e2 = new Etudiant(2,"KHALIL","BEN JEMAA");
+            Etudiant e3 = new Etudiant(3,"OLFA","BEN AFIA");
+            Etudiant e4 = new Etudiant(4,"SHIRAZ","BESROUR");
+
+
+            espVector.ajouterEtudiant(e1);
+            espVector.ajouterEtudiant(e2);
+            espVector.ajouterEtudiant(e4);
+            espVector.ajouterEtudiant(e3);
+
+            espVector.displayEtudiants();
+
+
+           */
     }
 }
+
+
+
+
+
+
+
